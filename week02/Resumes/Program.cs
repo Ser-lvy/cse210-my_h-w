e748d36dd959de444eb9fadd81a542403d1af15b
unknown <levimalesh10@gmail.com>
2026-09-08 17:48:53 +0300

    I completed the resume assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+
+        job1._jobTitle = "Software Engineer";
+        job2._jobTitle = "Digital Marketer";
+
+        job1._company = "Bank of Uganda";
+        job2._company = "China Town";
+
+        job1._startYear = 2026;
+        job2._startYear = 2027;
+
+        job1._endYear = 2030;
+        job2._endYear = 2031;
+
+
+
+        job1.DisplayJobDeatails();
+        Resume yourResume = new Resume();
+        yourResume._name = "Levi";
+
+        yourResume._jobs.Add(job1);
+        yourResume._jobs.Add(job2);
+
+        yourResume.DisplayJob();
+
+
+        
     }
 }
+
